7df073b34a05b62ebf631b71e07b6f34300dd91d
Dylan Turnbull <dylanturn@gmail.com>
2022-08-22 11:07:43 -0500

    adds support for subscriptions and groups

--- locals.tf
+++ locals.tf
@@ -1,6 +1,23 @@
 locals {
-  helm_parameters = {
+
+  operator_lifecycle_manager_parameters = {
     "namespaces.management" = var.namespaces.management
     "namespaces.operator"   = var.namespaces.operator
   }
+
+  operator_subscription_groups = ""
+
+
+
+  operator_subscription_parameters = [for operator_group, subscription in var.operator_subscriptions : {
+    name = subscription["name"]
+    namespace = subscription["namespace"]
+    channel = subscription["channel"]
+    operator = subscription["operator"]
+    source = subscription["operator"]
+    source_namespace = subscription["operator"]
+    install_plan_approval = subscription["operator"]
+    config = subscription["config"]
+    group = operator_group
+  }]
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,33 @@
 # The attributes of this object will become optional with inline defaults once Terraform 1.3.0 is released.
 variable "helm" {
   type = object({
-    name       = string
-    chart      = string
-    version    = string
-    repository = string
+    operator_lifecycle_manager = object({
+      name       = string
+      chart      = string
+      version    = string
+      repository = string
+    })
+    operator_subscription = object({
+      name       = string
+      chart      = string
+      version    = string
+      repository = string
+    })
   })
-  description = "ADD DESCRIPTION"
+  description = "An object that allows for the configuration of the helm chart used to deploy the lifecycle manager and operator subscriptions"
   default = {
-    name       = "operator-lifecycle-manager"
-    chart      = "./chart"
-    version    = "1.0.0"
-    repository = null
+    operator_lifecycle_manager = {
+      name       = "operator-lifecycle-manager"
+      chart      = "./charts/operator-lifecycle-manager"
+      version    = "0.22.0"
+      repository = null
+    }
+    operator_subscription = {
+      name       = "operator-lifecycle-manager"
+      chart      = "./charts/operator-subscription"
+      version    = "0.0.1"
+      repository = null
+    }
   }
 }
 
@@ -20,9 +36,47 @@ variable "namespaces" {
     management = string
     operator   = string
   })
-  description = "ADD DESCRIPTION"
+  description = "An object that defines the namespaces of the lifecycle management operator as well as other subscribed operators."
   default = {
     management = "olm"
     operator   = "operators"
   }
+}
+
+# The attributes of this object will become optional with inline defaults once Terraform 1.3.0 is released.
+variable "operator_subscription_groups" {
+  type = list(object({
+    group_name = string
+    group_namespace = string
+    group_upgrade_strategy = string
+    group_subscriptions = list(object({
+      name = string
+      operator = string
+      channel = string
+      source = string
+      starting_service_version = string
+      automatic_install_approval = bool
+      config = any
+    }))
+  }))
+  description = <<EOT
+  PUT DESCRIPTION HERE
+  EOT
+  default = []
+}
+
+# The attributes of this object will become optional with inline defaults once Terraform 1.3.0 is released.
+variable "operator_subscriptions" {
+  type = list(object({
+    name = string
+    namespace = string
+    operator = string
+    channel = string
+    source = string
+    starting_service_version = string
+    automatic_install_approval = bool
+    config = any
+  }))
+  description = "A map of {operator_group => subscription_config}"
+  default = {}
 }
